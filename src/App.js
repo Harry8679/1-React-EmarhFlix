9467abd3497cc2c835169e5b138dfc13cf5f5fa6
Harry MacCode <emarh.harry.code@gmail.com>
2025-05-16 15:44:39 +0200

    Comment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createGlobalStyle } from "styled-components";
+// import { createGlobalStyle } from "styled-components";
 import Home from "./Home";
 import { Router } from "@reach/router";
 
