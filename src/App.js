8536f9250e2718d27d0a757a34d00f794dd3585f
Harry MacCode <emarh.harry.code@gmail.com>
2025-05-16 15:38:42 +0200

    App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createGlobalStyle } from "styled-components";
+import Header from "./elements/Header";
+import Home from "./Home";
+import { Router } from "@reach/router";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+import { GlobalStyle } from "./styles/GlobalStyle";
+import Movie from "./Movie";
+import NotFound from "./NotFound";
+
+const App = () =>
+    <>
+        <Header />
+        <Router>
+            <Home path="/" />
+            <Movie path="/:movieId" />
+            <NotFound default />
+        </Router>
+        <GlobalStyle />
+    </>;
 
 export default App;
