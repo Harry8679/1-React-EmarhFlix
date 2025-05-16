cb21c9d9f30a9d9925e5d01e44a8dfa77d5b5c27
Harry MacCode <emarh.harry.code@gmail.com>
2025-05-16 15:44:21 +0200

    Home 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createGlobalStyle } from "styled-components";
-import Header from "./elements/Header";
 import Home from "./Home";
 import { Router } from "@reach/router";
 
@@ -8,6 +7,7 @@ import { Router } from "@reach/router";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import Movie from "./Movie";
 import NotFound from "./NotFound";
+import Header from './components/elements/Header';
 
 const App = () =>
     <>
